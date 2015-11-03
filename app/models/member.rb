46040b631be9e8b568f0c7aad4e6acd185dced51
Jeremiah Stump <stumpy224@gmail.com>
2015-11-02 20:58:50 -0500

    only return active members

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-  validates_presence_of :identifier, :name, :active
+  validates_presence_of :identifier, :name
   has_many :votes
   
   def active?
@@ -7,6 +7,6 @@ class Member < ActiveRecord::Base
   end
 
   def self.get_member_by_identifier(acid)
-    @member = Member.find_by identifier: acid.upcase
+    @member = Member.where(identifier: acid.upcase, active: true)
   end
 end

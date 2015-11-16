54a91cdf10062c849dcdb2175dfa09b75c9f7bec
Jeremiah Stump <stumpy224@gmail.com>
2015-11-16 10:16:31 -0500

    add meals resource

---
+++ app/views/meals/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @meal, :id, :member_id, :dinner_indicator, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cutoffs
+  resources :meals
   resources :members
   resources :nominations
   resources :restaurants
